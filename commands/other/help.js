945ee2feb7393201a8d969699d50df2b373ea024
Sylvernity <44215344+Sylvernity@users.noreply.github.com>
2024-09-06 20:04:30 -0400

    Added /help command

--- commands/other/help.js
+++ commands/other/help.js
@@ -1,13 +1,24 @@
 const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
-const client = require('bot.js');
-const fs = require('fs');
-const path = require("node:path");
+const client = require('../../client.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Displays information about Prowler\'s commands'),
     async execute(interaction) {
-        console.log(`${await client.application.commands.fetch()}`);
+        const cmdArray = Array.from(await client.commands.keys());
+        const embedArray = [];
+        for (const cmd of cmdArray) {
+            const command = client.commands.get(cmd);
+            embedArray.push({name: cmd, value: command.data.description});
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor(0x691514)
+            .setTitle(`Prowler Commands`)
+            .addFields(embedArray);
+
+        // 2. Print to user
+        await interaction.reply({embeds: [embed]});
     }
 }
