0b1cf6b4836db24dada6b7cc43cb5b44cae946ee
Sylvernity <44215344+Sylvernity@users.noreply.github.com>
2024-09-06 20:08:51 -0400

    Added client.js file

---
+++ client.js
@@ -0,0 +1,5 @@
+const { Client, GatewayIntentBits} = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions]});
+
+module.exports = client;
