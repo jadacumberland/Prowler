c59ba293b93721d6c14db901d12fbe965b97ee1e
Sylvernity <44215344+Sylvernity@users.noreply.github.com>
2024-09-08 01:44:11 -0400

    Fixed removeIGN array functionality and error message

--- commands/profile/removeIGN.js
+++ commands/profile/removeIGN.js
@@ -22,16 +22,33 @@ module.exports = {
 
         try {
             const file = JSON.parse(fs.readFileSync(jsonLocation, "utf-8"));
+            const array = file[`${userid}`][delGame];
+
             // Delete user game id
+            for (const index in array) {
+                if (array[index] === delName) {
+                    array.splice(index, 1);
+                    break;
+                }
+                if (index == array.length - 1) {
+                    throw new Error("Username not found.");
+                }
+            }
             delete file[`${userid}`][delGame][delName];
+
+            if (array === undefined || array.length === 0) {
+                // array does not exist or is empty
+                delete file [`${userid}`][delGame];
+            }
 
             // 2b. Save file.
             fs.writeFileSync(jsonLocation, JSON.stringify(file));
+            console.log(`[INFO] Username was successfully removed.`);
             interaction.reply({content: 'Username was successfully removed.', ephemeral: true})
         }
         catch (err) {
-            console.log(`ERROR: ${err}`);
-            interaction.reply({content: 'No matching game found. Check your spelling.', ephemeral: true});
+            console.log(`[ERROR] ${err}`);
+            interaction.reply({content: 'No matching game or username found. Check your spelling.', ephemeral: true});
         }
     }
 }
